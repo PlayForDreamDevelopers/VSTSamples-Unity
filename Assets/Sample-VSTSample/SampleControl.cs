4579cb0667680ff1236471709180817118c82561
yangrongZ <zhuyangrong.yvr.cn>
2025-05-23 14:24:15 +0800

    chore:  remove Enterprise and AD related packages
    DetailInfo: 删除 Enterprise 和 AD 相关的包

--- Assets/Sample-VSTSample/SampleControl.cs
+++ Assets/Sample-VSTSample/SampleControl.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using YVR.Core;
-using YVR.Enterprise.Camera;
 
 namespace YVR.Samples
 {
@@ -10,11 +9,6 @@ namespace YVR.Samples
         {
             YVRManager.instance.hmdManager.SetPassthrough(true);
             Debug.Log("Open SetPassthrough");
-            // YVRVSTCameraPlugin.SetVSTCameraFrequency(VSTCameraFrequencyType.VSTFrequency30Hz);
-            // YVRVSTCameraPlugin.SetVSTCameraResolution(VSTCameraResolutionType.VSTResolution660_616);
-            // YVRVSTCameraPlugin.SetVSTCameraFormat(VSTCameraFormatType.VSTCameraFmtNv21);
-            // YVRVSTCameraPlugin.SetVSTCameraOutputSource(VSTCameraSourceType.VSTCameraBothEyes);
-            // YVRVSTCameraPlugin.OpenVSTCamera();
         }
     }
 }
